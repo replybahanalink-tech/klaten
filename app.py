0f464fccc6824e493b34072136c15c0f51f43934
Sihr <replybahanalink@gmail.com>
2026-03-28 22:19:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -147,4 +147,4 @@ def create_ui():
 app = create_ui()
 
 if __name__ == "__main__":
-    app.launch(server_name="127.0.0.1", server_port=7860)
+    app.launch(server_name="0.0.0.0", server_port=7860, theme=gr.themes.Soft())
